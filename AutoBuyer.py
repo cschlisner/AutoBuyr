1a9cac254e3ec274da4605a29a455ca67b5ddef7
cschlisner <cschlisner@gmail.com>
2021-03-23 03:44:11 -0600

    Rework xpath storage

--- AutoBuyer.py
+++ AutoBuyer.py
@@ -1,109 +1,97 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import selenium_util as Sel
 from time import sleep 
 import threading 
+import json
 
+checkout_info = {
+    "CC" : "4111111111111111111",
+    "CVV": "123",
+    "EXPM": "08",
+    "EXPY": "2021",
+    "FName" : "FName", 
+    "LName" : "Lname",
+    "Addr": "123 Main St",
+    "State": "CO",
+    "City": "Denver",
+    "ZIP":"80103",
+    "Phone":"1234567891",
+    "Phone1":"123",
+    "Phone2":"456",
+    "Phone3":"7891",
+    "Email":"e@mail.com"
+}
 
-# XPath Crawler -- streamlines webpage traversal
-class XPathC:
-    _SoldOutDict = {
-        "bestbuy": ("//button[@class='btn btn-disabled btn-lg btn-block add-to-cart-button']", lambda e: e.text=="Sold Out"), 
-        "officedepot": ("//div[@class='deliveryMessage']/span", lambda e: e.text=="Out of stock for delivery"),
-        "amazon": ("/html/body/div[2]/div[2]/div[9]/div[3]/div[4]/div[19]/div[1]/span", lambda e: e.text=="Currently unavailable.")
-    }
-
-    _CheckoutPaths = {
-        "bestbuy" : [("//button[contains(@class,'add-to-cart-button')]", lambda e: e.send_keys(Keys.ENTER), 2), # add to cart
-                    ("href:/cart", None, 4), # go to cart
-                    ("//select[contains(@id, 'quantity')]", lambda e: e.send_keys("1"), 3), # make sure we only added one to cart
-                    ("/html/body/div[1]/main/div/div[2]/div[1]/div/div[1]/div[1]/section[2]/div/div/div[3]/div/div[1]/button", lambda e: e.click(), 4), # checkout
-                    ("//button[contains(@class, 'continue guest')]", lambda e: e.click(),3), #continue as guest 
-                    # Checkout Info Fields
-                    ("//input[@id='consolidatedAddresses.ui_address_2.firstName']", lambda e: sendAllKeys(e, checkout_info['FName']),2), 
-                    ("//input[@id='consolidatedAddresses.ui_address_2.lastName']", lambda e: sendAllKeys(e, checkout_info['LName']),2), 
-                    ("//input[@id='consolidatedAddresses.ui_address_2.street']", lambda e: sendAllKeys(e, checkout_info['Addr']),2), 
-                    ("//input[@id='consolidatedAddresses.ui_address_2.city']", lambda e: sendAllKeys(e, checkout_info['City']),2), 
-                    ("//select[@id='consolidatedAddresses.ui_address_2.state']", lambda e: [e.send_keys(Keys.DOWN) for k in range(9)],2), 
-                    ("//input[@id='consolidatedAddresses.ui_address_2.zipcode']", lambda e: sendAllKeys(e, checkout_info['ZIP']),2), 
-                    ("//input[@id='user.emailAddress']", lambda e: sendAllKeys(e, checkout_info['Email'])), 
-                    ("//input[@id='user.phone']", lambda e: sendAllKeys(e, checkout_info['Phone'])), 
-                    ("/html/body/div[1]/div[2]/div/div[2]/div[1]/div[1]/main/div[2]/div[2]/form/section/div/div[2]/div/div/button", lambda e: e.click())
-        ],
-        "officedepot": [("//input[@id='addToCartButtonId']", lambda e: e.click(), 3), 
-                        ("href:/cart/shoppingCart.do",None,4),
-                        ("//input[@id='quantity0']", lambda e: e.clear(), 1),
-                        ("//input[@id='quantity0']", lambda e: e.send_keys("1"), 1),
-                        ("//a[contains(@class,'checkout-btn')]", lambda e: e.click(), 3),
-                        ("/html/body/div[3]/div[3]/div/div/div/div[1]/div/div/div/div[2]/div/a", lambda e: e.click(), 3),
-                        #checkout info
-                        ("//input[@id='firstName-2']", lambda e: sendAllKeys(e, checkout_info['FName']),2), 
-                        ("//input[@id='lastName-2']", lambda e: sendAllKeys(e, checkout_info['LName']),2), 
-                        ("//input[@id='address1-2']", lambda e: sendAllKeys(e, checkout_info['Addr']),2), 
-                        ("//input[@id='postalCode1-2']", lambda e: sendAllKeys(e, checkout_info['ZIP']),2), 
-                        ("//input[@id='city-2']", lambda e: sendAllKeys(e, checkout_info['City']),2), 
-                        ("//select[@id='state-2']", lambda e: [e.send_keys(Keys.DOWN) for k in range(5)],2), 
-                        ("//input[@id='email-2']", lambda e: sendAllKeys(e, checkout_info['Email'])), 
-                        ("//input[@id='phoneNumber1-2']", lambda e: sendAllKeys(e, checkout_info['Phone'][:3])), 
-                        ("//input[@id='phoneNumber2-2']", lambda e: sendAllKeys(e, checkout_info['Phone'][3:6])), 
-                        ("//input[@id='phoneNumber3-2']", lambda e: sendAllKeys(e, checkout_info['Phone'][6:10])), 
-                        ("/html/body/div[1]/div[2]/div/div[2]/div[1]/div[1]/main/div[2]/div[2]/form/section/div/div[2]/div/div/button", lambda e: e.click())
-        ]
-    }
-
-    def sendAllKeys(self, element, text):
-        for k in checkout_info['Email']:
+# because element.send_keys() is broken?
+def sendAllKeys(element, text):
+        for k in text:
             element.send_keys(k) 
 
+# XPath Crawler -- streamlines webpage traversal
+class Site:
+    # full domain
     domain = ""
-
-    # SO = the sold out element. 
-    # SO[0] = xpath
-    # SO[1] = lambda indicating whether or not element matching xpath matches expected condition
-    # SO[1](driver.find_element_by_xpath(SO[0])) == True <=> product is sold out
-    SO = None
-
-    # list of tuples (xpath, lambda, ?wait) where lambda does something with the found element, and wait is an optional delay between steps
-    # p[0..n] = checkout traversal. p[i](driver.find_element_by_xpath(p[i])) advances the checkout process one step. 
-    p = []
-
-    def __init__(self, domain):
+    # Site pages for nav
+    pages = {}
+    # Site map
+    map = {}
+    # Site layout (page order)
+    layout = []
+    
+    def __init__(self, domain, sitemap_file="./PATH/sitemap.json"):
         self.domain = domain
-        self.SO = self._SoldOutDict[domain]
-        self.p = self._CheckoutPaths[domain]
+        with open(sitemap_file, "r") as f:
+            sitemap = json.load(f)
+            self.domain = sitemap[domain]["Domain"]
+            self.page = sitemap[domain]["Page"]
+            self.map = sitemap[domain]["Map"]
+            self.layout = sitemap[domain]["Layout"]
 
+    def url(self, i):
+        return self.domain+self.page[self.layout[i]]
+
+    
 
 class productMonitor(threading.Thread):    
     info = ""
-    status = 0
+    status = False
     URL = ""
     domain = ""
     driver = None
-    name = ""
-    xpathc = None
+    prodname = ""
+    site = None
     update_int = 1
-    autobuy = False
+    AUTO_PURCHASE = False
+    PURCHASE_INITIATED=False
     BOUGHT = False
-    def __init__(self, name, url, update_freq, auto_buy, headless=True):
+    EXCEPTION = None
+    def __init__(self, product_name, url, update_freq, auto_buy, headless=True):
         threading.Thread.__init__(self)
-        self.name = name
+        self.prodname = product_name
         self.URL = url
         self.domain = url.split('.')[1]
         self.driver = init(headless)
-        self.xpathc = XPathC(self.domain)
+        self.site = Site(self.domain)
         self.update_int = update_freq
-        self.autobuy = auto_buy
+        self.AUTO_PURCHASE = auto_buy
         self.stopped = False
         self.driver.get(url)
-        sleep(8)
-
+    
 
     def inStock(self):
+        page = list(filter(lambda p: p in self.driver.current_url, self.site.map))[0]
+        assert page in self.URL
+
         try:
-            el = self.driver.find_element_by_xpath(self.xpathc.SO[0])
-            return not self.xpathc.SO[1](el)
-        except NoSuchElementException:
-            self.info="No Such Element: "+self.xpathc.SO[0]
+            # self.site.map[page][0] will always be the sold out element 
+            e = self.driver.find_element_by_xpath(self.site.map[page][0]["p"])
+            return e == None
+        except NoSuchElementException as e:
+            self.info="No Such Element: "+self.site.map[page][0]['p']
             return True
 
     def update(self):
@@ -112,16 +100,33 @@ class productMonitor(threading.Thread):
     def run(self):
         while self.stopped==False:
             try:
-                if (self.inStock()):
-                    self.alert(self.name+" in stock @ "+self.URL, 1)
-                    if self.autobuy:
-                        self.buy()
-                else: self.info = ""
-                sleep(self.update_int)
-                self.update()
+                # get current url
+                loc = self.driver.current_url
+                for page in self.site.map:
+                    if page in self.URL:
+                        # we are on the product page
+                        # check stock and attempt checkout
+                        while (not self.inStock()):
+                            self.status = False
+                            self.info = ""
+                            sleep(self.update_int)
+                            self.update()
+
+                        # in stock now
+                        # self.alert(self.name+" in stock @ "+self.URL, True)
+                        if self.AUTO_PURCHASE:
+
+                            # Attempting Purchase
+                            self.PURCHASE_INITIATED = True
+                            try: 
+                                self.attempt_buy()
+                            except Exception as e:
+                                self.PURCHASE_INITIATED = False
+                                self.EXCEPTION = e
+                                self.kill()
             except Exception as e: 
-                self.info = str(e)
-                self.update()
+                self.EXCEPTION = e
+                self.kill()
     
     def kill(self):
         self.stopped = True
@@ -130,22 +135,70 @@ class productMonitor(threading.Thread):
         self.info = msg
         self.status = stat
 
-    def buy(self):
-        for e in self.xpathc.p:
-            try: 
-                if "href:" not in e[0]:
-                    e[1](self.driver.find_element_by_xpath(e[0]))
-                else:
-                    self.driver.get("https://www.{}.com{}".format(self.domain, e[0].split(":")[1]))
-                if len(e) > 2:
-                    sleep(e[2])
-            except NoSuchElementException:
-                self.info="No Such Element: "+e[0]
+    def processElem(self, element):
+        xpath = element["p"]
+        click = "c" in element and element["c"]
+        inputText = None if "it" not in element else element["it"]
+        expectedText = None if "et" not in element else element["et"]
+        notPresent = "np" in element and element["np"]
+
+        if "iframe" in element:
+            self.driver.switch_to.frame(element["iframe"])
+
+        if "w" in element:
+            self.driver.implicitly_wait(element["w"])
+        try:
+            el = Sel.wait_for_element_by_xpath(self.driver, xpath, 1 if notPresent else 30)
+            # el = self.driver.find_element_by_xpath(xpath) 
+        except Exception as e:
+            if notPresent:
                 return
-        self.autobuy = False
-        self.alert("BOUGHT {} @ {} for {}".format(self.name, self.URL, "price"), 0)
-        self.BOUGHT = True
-        self.stopped = True
+            else:
+                raise e
+        if inputText is not None:
+            if "CI" in inputText:
+                inputText = checkout_info[inputText.split(".")[1]]
+            if "select[" in element["p"]:
+                select = Select(el)
+                try:
+                    select.select_by_value(inputText)
+                except:
+                    select.select_by_visible_text(inputText)
+            else:
+                try:
+                    el.clear()
+                except:
+                    pass
+                el.send_keys(inputText)
+        if expectedText is not None:
+            if element["p"][:7] == "//input":
+                #check value instead of text content
+                assert el.get_attribute('value') == expectedText
+            else: assert el.text == expectedText
+        if click:
+            Sel.button_click_using_xpath(self.driver,xpath)
+        
+        if "iframe" in element:
+            self.driver.switch_to.default_content()
+
+    def attempt_buy(self):
+        pagei = 0
+        purchase_complete = False
+        # Out of stock should be gone
+
+        while not purchase_complete:
+            for page in self.site.map:
+                if page in self.driver.current_url:
+                    # process elements on our page (in order)
+                    for element in self.site.map[page]:
+                        print("processing {}".format(element['label']))
+                        self.processElem(element)
+                        if element["label"] == "ConfirmOrder":
+                            purchase_complete = True
+                    self.driver.get(self.site.url(pagei))
+                    sleep(2)
+                    pagei += 1
+
 
             
 def init(headless):
@@ -158,28 +211,39 @@ def init(headless):
 
 
 products = {
-    'Test': ["https://www.bestbuy.com/site/whirlpool-5-1-cu-ft-freestanding-gas-range-stainless-steel/4146081.p?skuId=4146081", "https://www.officedepot.com/a/products/434207/HP-950XL951-BlackCyanMagentaYellow-Ink-Cartridges-C2P01FNM/?cm_cat=513382"],
-    '3060ti': ["https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402", "https://www.bestbuy.com/site/evga-nvidia-geforce-rtx-3060-ti-ftw3-gaming-8gb-gddr6-pci-express-4-0-graphics-card/6444444.p?skuId=6444444", "https://www.officedepot.com/a/products/3973362/PNY-GeForce-RTX-3060-Ti-8GB"],
-    '3060':["https://www.officedepot.com/a/products/8113849/PNY-GeForce-RTX-3060-12GB-XLR8/", ]
+    'TestOD': ["https://www.officedepot.com/a/products/690690/WorkPro-Quantum-9000-Mesh-Series-High/"],
+    # 'TestBB': ["https://www.bestbuy.com/site/whirlpool-5-1-cu-ft-freestanding-gas-range-stainless-steel/4146081.p?skuId=4146081"],
+    #'3060ti': ["https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402", "https://www.bestbuy.com/site/evga-nvidia-geforce-rtx-3060-ti-ftw3-gaming-8gb-gddr6-pci-express-4-0-graphics-card/6444444.p?skuId=6444444", "https://www.officedepot.com/a/products/3973362/PNY-GeForce-RTX-3060-Ti-8GB"],
+    #'3060':["https://www.officedepot.com/a/products/8113849/PNY-GeForce-RTX-3060-12GB-XLR8/", ]
 }
+
+def alert(monitor):
+    pass
+
 
 from reprint import output
 import random
 import time
+import traceback
 if __name__ == '__main__':
     monitors = []
     for prod in products:
         for page in products[prod]:
-            monitors.append(productMonitor(prod, page, 3, False))
+            monitors.append(productMonitor(prod, page, 3, True, headless=False))
     for monitor in monitors:
         monitor.start()   
     stoppedc = 0 
-    with output(initial_len=len(monitors)+1, interval=0) as output_lines:
-        while stoppedc < len(monitors):
-            output_lines[0] = "{}\t|\t{}\t|\t{}{}".format("Product", "In Stock", "Running Threads: ", threading.activeCount())
-            for i, m in enumerate(monitors):
-                output_lines[i+1] = "{}({})\t|\t{}\t|\t{}".format(m.name, m.domain, bool(m.status), m.info)
-                if m.stopped: stoppedc += 1
-            
+    # with output(initial_len=len(monitors)+1, interval=50) as output_lines:
+    while stoppedc < len(monitors):
+            # output_lines[0] = "{}\t|\t{}\t|\t{}{}".format("Product", "In Stock", "Running Threads: ", threading.activeCount())
+        for i, m in enumerate(monitors):
+            # output_lines[i+1] = "{}({})\t|\t{}\t|\t{}".format(m.name, m.domain, m.status, m.info)
+            if m.status:
+                alert(m)
+            if m.stopped: stoppedc += 1
+            if m.EXCEPTION is not None:
+                print("\n\n")
+                traceback.print_exception(type(m.EXCEPTION), m.EXCEPTION, m.EXCEPTION.__traceback__)
+
         
     
